9771ff68065a2d3da65e367ce6ce366ad1f8108f
Valet78 <valet78@mail.ru>
2022-09-02 17:43:34 +0300

    Решение первой задачи семинара

--- Zadacha_1/Program.cs
+++ Zadacha_1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимае число и выдаёт его квадрат 
+
+ Console.Write("Введите целое число: ");
+ int number = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine($"Квадрат введённого Вами числа {number} равен {number * number}");
+   
