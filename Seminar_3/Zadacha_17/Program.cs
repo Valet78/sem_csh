617e500615c9ee9e4a7161909dea8f8bcb3ef5d5
Valet78 <valet78@mail.ru>
2022-09-05 15:33:21 +0300

    тренировка

---
+++ Seminar_3/Zadacha_17/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+public class MainClass
+{
+    
+    public static void Main()
+    {
+        string num_str = "5.25";
+        System.Globalization.NumberFormatInfo numFormat = new System.Globalization.NumberFormatInfo();
+        numFormat.NumberDecimalSeparator = ".";
+        float number = float.Parse(num_str, numFormat);
+        Console.WriteLine(number);
+        
+    }
+}
