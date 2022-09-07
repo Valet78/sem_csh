985b3f6a3e18ae9ae986d5d8ff8649a7e197194b
Valet78 <valet78@mail.ru>
2022-09-07 10:57:03 +0300

    Решение 23 задачи ДЗ к семинару 3

---
+++ Seminar_3/Zadacha_23dz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+public class MainClass
+{
+    
+    public static void Main()
+    {
+        Console.Clear();
+        
+      // *********************  
+        
+        Console.Write("Введите целое число больше нуля: ");
+        int num = Convert.ToInt16(Console.ReadLine ());
+        
+        if (num > 0) {
+            for(int i=1; i <= num; i++){
+                Console.Write(i*i*i);
+            if (i < num) Console.Write(", ");
+            }
+        } else Console.WriteLine("Введено недопустимое значение!");
+
+    }
+}
+
+
+/*
+
+Задача 23: Напишите программу, которая принимает на вход число (N) и 
+выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+
+
+*/
