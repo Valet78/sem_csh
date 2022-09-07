24cf8ad015e989a2711949ec139b3f17a07469f9
Valet78 <valet78@mail.ru>
2022-09-07 08:44:59 +0300

    Решение 17 задачи 3 семинара

--- Seminar_3/Zadacha_17/Program.cs
+++ Seminar_3/Zadacha_17/Program.cs
@@ -1,15 +1,52 @@
 ﻿using System;
-using System.Globalization;
 public class MainClass
 {
     
     public static void Main()
     {
-        string num_str = "5.25";
-        System.Globalization.NumberFormatInfo numFormat = new System.Globalization.NumberFormatInfo();
-        numFormat.NumberDecimalSeparator = ".";
-        float number = float.Parse(num_str, numFormat);
-        Console.WriteLine(number);
+        Console.Clear();
         
+      // *********************  
+        
+        Console.WriteLine("Введите координаты точки (где X ≠ 0 и Y ≠ 0)");
+        Console.Write("x = ");
+        int?  x = Convert.ToInt16(Console.ReadLine());
+        Console.Write("y = ");
+        int?  y = Convert.ToInt16(Console.ReadLine());
+        
+        if (x != 0 && y != 0) {
+            if (x > 0 && y > 0){
+                  Console.WriteLine($"Точка с координатами ({x}, {y}) находится в первой четверти");
+             }
+            if (x < 0 && y > 0){
+                  Console.WriteLine($"Точка с координатами ({x}, {y}) находится во второй четверти");
+             }
+            if (x < 0 && y < 0){
+                  Console.WriteLine($"Точка с координатами ({x}, {y}) находится в третьей четверти");
+             }
+            if (x > 0 && y < 0){
+                  Console.WriteLine($"Точка с координатами ({x}, {y}) находится в четвёртой четверти");
+             }
+
+        }
+          else Console.WriteLine("Введено недопустимое значение!");
+
+
+
+        
+
+
+
     }
 }
+
+
+/*
+
+Напишите программу, которая принимает на вход
+координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+номер четверти плоскости, в которой находится эта
+точка.
+
+*/
+
