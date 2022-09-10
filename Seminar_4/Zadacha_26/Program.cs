b0ac53748225975c4816e92deb57029e95ef9254
Valet78 <valet78@mail.ru>
2022-09-10 22:28:46 +0300

    Решение 26 задачи семинара 4

---
+++ Seminar_4/Zadacha_26/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+
+*/
+
+
+using System;
+
+public class MainClass {
+    public static void Main(){
+            Console.Clear();
+
+            Console.WriteLine("Введите целое число:");
+            string ? number_str = Console.ReadLine();
+            
+            if (number_str != "") {
+                int simvl = LegthString (number_str);
+                if (simvl != -1) Console.WriteLine($"В числе {number_str} цифр {simvl}");       
+                else Console.WriteLine("Введено некорректное число.");    
+            }
+            else Console.WriteLine("Вы забыли ввести число. Попробуйте снова.");
+                        
+                        
+            int LegthString (string num_str){
+                int res = num_str.Length;                
+                int num = 0;
+                
+                try {
+                    num = Convert.ToInt32(num_str);
+                    if (res == 1 && !(num >=0 && num <= 9)) res = -1;
+                    if (res > 1 && num < 0) res -= 1;
+                }
+                catch (Exception){
+                    res = -1;
+                }            
+
+                return res;
+            }
+               
+    }
+
+            
+
+}
+
