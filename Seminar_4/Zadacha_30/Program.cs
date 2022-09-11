e006fa82d986d23670e907bfef2646e5da682b44
Valet78 <valet78@mail.ru>
2022-09-11 20:48:33 +0300

    Решение задачи 30 семинара 4

---
+++ Seminar_4/Zadacha_30/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный
+нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+
+
+*/
+
+
+using System;
+
+public class MainClass {
+    public static void Main(){
+            Console.Clear();
+            int elem = 8;
+            int [] number = new int [elem];
+
+            for(int i=0; i < elem; i++){
+                RndArray(i);
+            }
+
+            Console.Write("[");
+            for (int i=0; i < elem; i++){
+                Console.Write(number[i]);
+                if (i<7) Console.Write(",");
+            }
+            Console.WriteLine("]");
+
+            void RndArray(int ind){
+                number[ind] = new Random().Next(0, 2);
+            }               
+    }           
+
+}
+    
