586d077786dbad9690c7a8016db54bc43e13ad12
Valet78 <valet78@mail.ru>
2022-09-12 12:46:00 +0300

    Решение задачи 29 ДЗ к семинару 4

---
+++ Seminar_4/Zadacha_29dz/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+
+*/
+
+
+using System;
+
+public class MainClass {
+    public static void Main(){
+            Console.Clear();
+            
+            int [] mass = new int [8];
+            string temp="[";
+            for(int i = 0; i < 8; i++){
+                mass[i] = new Random().Next(100);
+                temp += mass[i].ToString();
+                if(i < 7) temp += ", ";
+            }         
+            Console.Write(temp + "]");
+    }           
+
+}
+    
