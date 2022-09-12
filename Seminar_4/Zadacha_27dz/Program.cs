2274507d3ee9f5f76983dfe0c3b486c93a79ebf6
Valet78 <valet78@mail.ru>
2022-09-12 12:25:36 +0300

    Решение задачи 27 ДЗ к семинару 4

---
+++ Seminar_4/Zadacha_27dz/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12
+
+*/
+
+
+using System;
+
+public class MainClass {
+    public static void Main(){
+            Console.Clear();
+            
+            Console.WriteLine("Введите целое число:");
+            string ? num_str = Console.ReadLine();
+
+            if (num_str != ""){               
+                int len = num_str.Length;
+                int sum = 0, temp = 0;
+                for(int i = 0; i < len; i++){
+                    if(num_str[i] == '-') continue;
+                    temp = TryInputText (num_str[i].ToString());
+                    if(temp != -1) sum += temp; 
+                    else Console.WriteLine("Введено некорректное число. Попробуйте снова.");
+                }
+                Console.WriteLine($"Сумма цифр в числе {num_str} равна {sum}");                
+                }
+                else Console.WriteLine("Введено некорректное число. Попробуйте снова.");
+            
+
+                        // Проверка правильности ввода числа   
+            int TryInputText (string InputText){
+                int try_res=0;
+                if(!int.TryParse(InputText, out try_res)) try_res = -1;
+                return try_res;
+            }            
+    }           
+
+}
+    
