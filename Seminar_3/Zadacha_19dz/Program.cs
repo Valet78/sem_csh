5d7ceeca8c04b13c82b32a096bd439cae917e9b0
Valet78 <valet78@mail.ru>
2022-09-16 10:38:58 +0300

    Редактирование задачи 19 ДЗ к семинару 3

--- Seminar_3/Zadacha_19dz/Program.cs
+++ Seminar_3/Zadacha_19dz/Program.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/*
+Задача 19: Напишите программу, которая принимает на вход пятизначное число 
+и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+23432 -> да
+12821 -> да
+
+*/
+
+
+using System;
 public class MainClass
 {
     
@@ -10,37 +21,26 @@ public class MainClass
         
         Console.Write("Введите целое число: ");
         string ? num_str = Console.ReadLine ();
-        int flag = 0, start = 0, len = num_str.Length-1;
-        int num = Convert.ToInt32(num_str);
+        int flag = 0, start = 0, len = 0; 
+        if (num_str != ""){
+            len = num_str!.Length-1;
+            int num = Convert.ToInt32(num_str);
 
-        if (num < -9 || num > 9){
-            if (num < -9) start = 1;
-            for (int i=start; i <= len/2; i++){
-                
+            if (num < -9 || num > 9){
+                if (num < -9) start = 1;
+                for (int i=start; i <= len/2; i++){                
                 if(num_str[i] != num_str[len-i+start]) {
                     flag=-1;
                     break;
                 }
 
             }
-            if(flag == -1) Console.WriteLine($"Число {num_str} не является палиндромом.");
-            else if(flag == 0) Console.WriteLine($"Число {num_str} палиндром.");
-
-        }
-        else Console.WriteLine("Введено некоректное значение! Цифр в числе должно быть более одной.");        
-
+                if(flag == -1) Console.WriteLine($"Число {num_str} не является палиндромом.");
+                else if(flag == 0) Console.WriteLine($"Число {num_str} палиндром.");
+            }
+            else Console.WriteLine("Введено некоректное значение! Цифр в числе должно быть более одной."); 
+        }       
+        else Console.WriteLine("Введено некорректное число.");
     }
 }
 
-
-/*
-
-Задача 19: Напишите программу, которая принимает на вход пятизначное число 
-и проверяет, является ли оно палиндромом.
-
-14212 -> нет
-23432 -> да
-12821 -> да
-
-
-*/
