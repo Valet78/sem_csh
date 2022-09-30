20a8868ebe03787c125c6fa0a33f544241a6c98b
Valet78 <valet78@mail.ru>
2022-09-30 22:53:40 +0300

    Коррекция решения задачи 64 ДЗ к семинару 9

--- Seminar_9/Zadacha_64dz/Program.cs
+++ Seminar_9/Zadacha_64dz/Program.cs
@@ -14,26 +14,24 @@ public class MainClass{
         
         Console.WriteLine("Введите число N для получения всех чётных чисел в промежутке от N до 1");
         int numN = inputNum();
-        if(numN > 2) NumRangeOut (numN);
-        else Console.WriteLine($"В промежутке между 1 и {numN} нет натуральных чисел. Попробуйте снова и введите число больше 2.");
+        if (numN > 2) {            
+            Console.Write($"N = {numN} -> \"{NumRangeOut ((numN % 2 != 0) ? -- numN : numN, Convert.ToString(numN))}\"");                    
+        } else Console.WriteLine($"В промежутке между 1 и {numN} нет натуральных чисел. Попробуйте снова и введите число больше 2.");
         Console.WriteLine("");
 
 
-
-        void NumRangeOut (int num){
-            string numStr = String.Empty;
-            Console.Write($"N = {numN} -> \"");
-            for(int i = numN; i >= 1; i--){
-                if (i % 2 == 0) {
-                    numStr += Convert.ToString(i);
-                    if(i > 2) numStr += ", ";
-                }
+            // Вычисление чётных чисел в промежутке от N до 1
+        string NumRangeOut (int num, string numStr){
+            if (num > 2){
+                num --;
+                if (num % 2 == 0) numStr += NumRangeOut (num, Convert.ToString(num));
+                 else numStr += NumRangeOut (num --, ", ");               
             }
-            Console.Write($"{numStr}\"");
-        }        
-       
-        // Ввод числа
-       int inputNum(){
+            return numStr;
+        } 
+
+            // Ввод числа
+        int inputNum(){
             int temp = 0;
             Console.Write("N = ");
             while (!TryInputText (Console.ReadLine() ?? String.Empty, ref temp)){
@@ -42,9 +40,9 @@ public class MainClass{
                 temp = 0;
             }
             return temp;
-       }
+        }
 
-       // Проверка правильности ввода числа   
+            // Проверка правильности ввода числа   
         bool TryInputText (string inputText, ref int inputInt){
             bool tryRes = true;            
             if(!Int32.TryParse(inputText, out inputInt)) tryRes = false; 
