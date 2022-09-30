2fe68f5e870b92d28b08a4c735969940ea87ec4a
Valet78 <valet78@mail.ru>
2022-09-30 19:12:08 +0300

    Решение задачи 69 семинара 9

---
+++ Seminar_9/Zadacha_69/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8
+
+*/
+using System;
+
+public class MainClass{
+    public static void Main(){
+        Console.Clear();
+
+        
+        Console.WriteLine("Введите числа A и B для получения значения степени B по основанию A");
+        int numA = inputNum("A = ");        
+        int numB = inputNum("B = ");
+        
+        if (numB != 0){
+            if (numB < 0) numB = Math.Abs(numB);            
+            double degree = DegreeOfNumber(numA, numB);
+            if (numB < 0) degree = 1 / degree;
+            Console.WriteLine($"A = {numA}; B = {numB} -> {degree}");
+        }   else Console.WriteLine($"A = {numA} в степени B = 0 всегда равно 1");
+        
+        Console.WriteLine("");
+
+
+        int DegreeOfNumber (int num, int deg){             
+            if (deg > 1)num *= DegreeOfNumber (num, -- deg); 
+            return num;
+        }
+       
+        // Ввод числа
+       int inputNum(string text){
+            int temp = 0;
+            bool tryNum = false;
+            string ? zapros = String.Empty;
+            
+            while (!tryNum){
+                Console.Write(text);
+                zapros = Console.ReadLine();
+                if (zapros != String.Empty) {
+                    tryNum = TryInputText (zapros ?? String.Empty, ref temp);
+                    tryNum = true;                   
+                } else {
+                    tryNum = false;
+                    Console.WriteLine("Введено некорректное число. Попробуйте снова.");                    
+                    }                             
+            }
+            return temp;
+       }
+
+       // Проверка правильности ввода числа   
+        bool TryInputText (string inputText, ref int inputInt){
+            bool tryRes = true;            
+            if(!Int32.TryParse(inputText, out inputInt)) tryRes = false; 
+            if (inputInt < 1)tryRes = false;                                
+            return tryRes;
+        }
+    }
+}
